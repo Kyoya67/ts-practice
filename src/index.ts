fd796f2cf25d4985a0fc51fd26983db07eedded5
kyoya <kyoya@kyoyanoMacBook-Air.local>
2025-08-04 19:42:55 +0900

    feata: Array type test

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ type User = {
 const john: User = {
   name: "John",
   age: 30,
-//   email: "john@example.com",
+  // email: "john@example.com",
 };
 
 const jane = {
@@ -19,3 +19,32 @@ const jane2 : User = jane;
 
 console.log(john);
 console.log(jane2);
+
+const users: Array<User &{ 
+  sex: "male" | "female",
+  place: string
+  }> = [
+  {
+    name: "John",
+    age: 30,
+    sex: "male",
+    place: "New York",
+  },
+  {
+    name: "Jane",
+    age: 25,
+    sex: "female",
+    place: "London",
+  },
+];
+
+console.log("users before", users);
+
+users[0] = {
+  name: "John2",
+  age: 309,
+  sex: "male",
+  place: "New York2",
+};
+
+console.log("users after", users);
