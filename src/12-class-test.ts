f99b005017edc26212a525532bcaabf9a1f7fbb9
kyoya67 <jackma8569@gmail.com>
2025-08-09 12:59:33 +0900

    feat: add class expression private test

--- src/12-class-test.ts
+++ src/12-class-test.ts
@@ -10,5 +10,22 @@ class User {
     }
 }
 
-const josh = new User("josh", 12);
-console.log(josh.isAdult());
+const Josh = new User("Josh", 12);
+console.log(Josh.isAdult());
+
+const UserExp = class{
+    public name: string;
+    private age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public isAdult(): boolean {
+        return this.age >= 20;   
+    }
+}
+
+const Takumu = new UserExp("Takumu", 23);
+console.log(Takumu.isAdult());
