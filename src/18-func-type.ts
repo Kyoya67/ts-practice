bd1e5bb344286ddd2f4f1b4613e605384d604b37
kyoya67 <jackma8569@gmail.com>
2025-08-13 18:11:45 +0900

    feat: add literal widerable test

---
+++ src/18-func-type.ts
@@ -0,0 +1,4 @@
+type F = (arg: string, arg2: string) => void;
+
+const fun: F = (str1, str2) => console.log(str1 + str2);
+fun("domo","ore");
---
+++ src/19-literal-widening.ts
@@ -0,0 +1,8 @@
+const uhyo1 = "uhyo";
+const uhyo2: "uhyo" = "uhyo";
+
+let uhyo3 = uhyo1;
+let uhyo4 = uhyo2;
+
+uhyo3 = "domo";
+uhyo4 = "domo";
