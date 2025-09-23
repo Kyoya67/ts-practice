96c9c87fbe7d784f222e0182a8e0b8bfa1dbbe6e
kyoya67 <jackma8569@gmail.com>
2025-09-23 17:31:06 +0900

    feat: brashup mapOption

--- src/28-6.8.5-test.ts
+++ src/28-6.8.5-test.ts
@@ -9,16 +9,29 @@ function doubleOption(obj: Option<number>): number | undefined {
     return mapOption(obj, x => x * 2);  
 }
 
+function toStringOption(obj: Option<number>): string | undefined {
+    return mapOption(obj, x => x.toString());
+}
+
+function toBigIntOption(obj: Option<number>): bigint | undefined {
+    return mapOption(obj, x => BigInt(x));
+}
+
 function isSome<T>(obj: Option<T>): obj is Exclude<Option<T>, {tag: "none"}> {
     return obj.tag === "some";
 }
 
-function mapOption(obj: Option<number>, fn: (num: number) => number): number | undefined{
+function mapOption<T, U>(obj: Option<T>, callback: (num: T) => U): U | undefined{
     if(isSome(obj)) { 
-        return fn(obj.value);
+        return callback(obj.value);
     }
+    return undefined;
 } 
 
 console.log(doubleOption({tag: "some", value: 8}));
+console.log(toStringOption({tag: "some", value: 9}));
+console.log(toBigIntOption({tag: "some", value: 10}));
 console.log(doubleOption({tag: "none"}));
+console.log(toStringOption({tag: "none"}));
+console.log(toBigIntOption({tag: "none"}));
 
